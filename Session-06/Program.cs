b00381aac39425203e272422083c5ca9d8f3bddf
Katerina <Kater@LAPTOP-735L895G>
2023-01-19 16:25:06 +0200

    new

--- Session-06/Program.cs
+++ Session-06/Program.cs
@@ -7,45 +7,109 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Session_06;
 
-class University //if i suppress can i take null or not
+class Institute {
 
-{ public Student[] Students { get; set; }
-    public Course[]? Courses { get; set; }
-    public Grade[]? Grades { get; set; }
-    public ScheduledCourse[]? ScheduledCourses { get; set; }
+    public Guid ID { get; set; }
+    string Name;
+    int YearsInService;
 
-    private void GetStudents(){
-
-    
-    }
-    private void SetCourses() { 
-    
-    }
-    private void GetGrades()
+    public void GetName()
     {
 
-
     }
-    private void SetSchedule( int CourseID, string Professor, int datetime)
+    public void SetName(string name)
     {
 
+    }
+
+
+    class University : Institute //if i suppress can i take null or not
+
+    {
+        private ScheduledCourse[]? schedule;
+
+        public Students[] Student { get; set; }
+        public Courses[]? Course { get; set; }
+        public Grades[]? Grade { get; set; }
+        public ScheduledCourse[]? Schedule { get => schedule; set => schedule = value; }
+
+        private void GetStudents()
+        {
+
+        }
+        private void GetCourses()
+        {
+
+        }
+        private void GetGrades()
+        {
+
+        }
+        private void SetSchedule(int CourseID, int ProfessorID, int datetime)
+        {
+
+
+        }
 
     }
 
-    }
+    public class Schedule { }
+   // mou vgazei error pws uparxei hdh kai den me afhnei na to ksanavalw
+   //public Guid ID;
+    public Guid CourseID { get; set; }
+    public Guid ProfessorID { get; set; }
 
+    // public void Calendar[]? DateandTime {}
+
+
+
+    public class Person {
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    
+
+    public Person(Guid id, string name) { }
+    
+      
+   
+
+public class Professor
+    {
+        public string Rank;
+        public Course[]? Courses { get; set; }
+
+        private void Teach(string course, int datetime)
+        {
+
+        }
+
+        private void SetGrade(int StudentID, int courseID, int grade)
+        {
+
+        }
+        private void Getname()
+        {
+
+        }
+    }
 
 }
+
+
+
 public class ScheduledCourse
 {
 
 }
 
-public class Grade
+public class Grades
 {
+    
 }
 
-public class Course
+public class Courses
 {
     int ID;
     string Code;
@@ -53,19 +117,33 @@ public class Course
 
 }
 
-public class Student
+public class Students : Person 
 {
     int RegistrationNumber;
+
+    public Students(Guid id, string name) : base(id, name)
+    {
+    }
+
     public Course[]? Courses { get; set; }
 
     private void Attend(string course, int datetime)
     {
 
     }
-    private void WriteExam( string course, int time) { 
-    
-    
+    private void WriteExam(string course, int time) {
+
+
+
+
     }
 
-};
+}
 
+public class Course
+{
+}
+
+
+
+}
